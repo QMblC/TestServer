87189dbeb67cb2a16bee2c8b858d7776f8beece9
CatWithoutSoup <roma.malov2000@mail.ru>
2023-11-09 15:16:35 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 80);
+            listener.Start();
+            var client = listener.AcceptTcpClient();
+            using (var stream = client.GetStream())
+            {
+                using (var reader = new StreamReader(stream))
+                using (var writer = new StreamWriter(stream))
+                {
+                    for (string? line = null; line != string.Empty; line = reader.ReadLine())
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    writer.WriteLine("We are Progers");
+                }
+            }
+        }
+    }
+}
